ef50c4eaad1590f35aa51a53f5d7276f6cd66273
David Soff <david@soff.nl>
2020-03-24 16:24:06 +0100

    Finish body cleanup pipeline

--- crawlinator/pipelines.py
+++ crawlinator/pipelines.py
@@ -6,7 +6,8 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from elastic_app_search import Client
-from scrapy.utils.markup import remove_tags
+from scrapy.utils.markup import replace_tags, remove_tags_with_content, replace_escape_chars
+from scrapy.selector import Selector
 import logging
 class appSearchPipeline(object):
     def process_item(self, item, spider):
@@ -22,6 +23,12 @@ class appSearchPipeline(object):
         return item
 
 class bodyCleanupPipeline(object):
+
     def process_item(self, item, spider):
-        item['body'] = remove_tags(item['body'])
+        body_only = Selector(text=item['body']).css('body').get()
+        script_removed = remove_tags_with_content(body_only, which_ones=('style', 'script'))
+        tags_replaced = replace_tags(script_removed, ' ')
+        item['body'] = replace_escape_chars(tags_replaced, ' ')
+
+        logging.info(f'Item cleaned up: {item["title"]}')
         return item
