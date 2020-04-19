398559e1dc7ef2802915aea6afb8d08592176511
griends1 <Steelpan1>
2020-04-19 20:50:56 +0200

    Tweaks CSS and Displaying results

--- crawlinator-ui/src/App.js
+++ crawlinator-ui/src/App.js
@@ -2,94 +2,94 @@ import React from "react";
 
 import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
 import CSS from './App.module.css'
+import ResultView from './ResultView'
 
 import {
-  ErrorBoundary,
-  Facet,
-  SearchProvider,
-  SearchBox,
-  Results,
-  PagingInfo,
-  ResultsPerPage,
-  Paging,
-  Sorting,
-  WithSearch
+    ErrorBoundary,
+    Facet,
+    SearchProvider,
+    SearchBox,
+    Results,
+    PagingInfo,
+    ResultsPerPage,
+    Paging,
+    Sorting,
+    WithSearch
 } from "@elastic/react-search-ui";
-import { Layout } from "@elastic/react-search-ui-views";
+import {Layout} from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
 
 import {
-  buildAutocompleteQueryConfig,
-  buildFacetConfigFromConfig,
-  buildSearchOptionsFromConfig,
-  buildSortOptionsFromConfig,
-  getConfig,
-  getFacetFields
+    buildAutocompleteQueryConfig,
+    buildFacetConfigFromConfig,
+    buildSearchOptionsFromConfig,
+    buildSortOptionsFromConfig, getBodyField,
+    getConfig,
+    getFacetFields, getTitleField
 } from "./config/config-helper";
 
-const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
+const {hostIdentifier, searchKey, endpointBase, engineName} = getConfig();
 const connector = new AppSearchAPIConnector({
-  searchKey,
-  engineName,
-  hostIdentifier,
-  endpointBase
+    searchKey,
+    engineName,
+    hostIdentifier,
+    endpointBase
 });
 const config = {
-  searchQuery: {
-    facets: buildFacetConfigFromConfig(),
-    ...buildSearchOptionsFromConfig()
-  },
-  autocompleteQuery: buildAutocompleteQueryConfig(),
-  apiConnector: connector,
-  alwaysSearchOnInitialLoad: true
+    searchQuery: {
+        facets: buildFacetConfigFromConfig(),
+        ...buildSearchOptionsFromConfig()
+    },
+    autocompleteQuery: buildAutocompleteQueryConfig(),
+    apiConnector: connector,
+    alwaysSearchOnInitialLoad: true
 };
 
 export default function App() {
-  return (
-    <SearchProvider config={config}>
-      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
-        {({ wasSearched }) => {
-          return (
-            <div className="App">
-              <ErrorBoundary>
-                <Layout
-                  header={<SearchBox autocompleteSuggestions={true} />}
-                  sideContent={
-                    <div className={CSS.Sorting}>
-                      {wasSearched && (
-                        <Sorting
-                          label={"Sort by"}
+    return (
+        <SearchProvider config={config}>
+            <WithSearch mapContextToProps={({wasSearched}) => ({wasSearched})}>
+                {({wasSearched}) => {
+                    return (
+                        <div className="App">
+                            <ErrorBoundary>
+                                <Layout
+                                    header={<SearchBox autocompleteSuggestions={true}/>}
+                                    sideContent={
+                                        <div>
+                                            {wasSearched && (
+                                                <Sorting
+                                                    label={"Sort by"}
 
-                          sortOptions={buildSortOptionsFromConfig()}
-                        />
-                      )}
-                      {getFacetFields().map(field => (
-                        <Facet key={field} field={field} label={field}  />
-                      ))}
-                    </div>
-                  }
-                  bodyContent={
-                    <Results className={CSS.Result}
-                      titleField={getConfig().titleField}
-                      urlField={getConfig().urlField}
-                      bodyField={getConfig().bodyField}
-                      shouldTrackClickThrough={true}
-
-                    />
-                  }
-                  bodyHeader={
-                    <React.Fragment>
-                      {wasSearched && <PagingInfo />}
-                      {wasSearched && <ResultsPerPage />}
-                    </React.Fragment>
-                  }
-                  bodyFooter={<Paging/>}
-                />
-              </ErrorBoundary>
-            </div>
-          );
-        }}
-      </WithSearch>
-    </SearchProvider>
-  );
+                                                    sortOptions={buildSortOptionsFromConfig()}
+                                                />
+                                            )}
+                                            {getFacetFields().map(field => (
+                                                <Facet key={field} field={field} label={field}/>
+                                            ))}
+                                        </div>
+                                    }
+                                    bodyContent={
+                                        <Results className={CSS.Result}
+                                                 titleField={getConfig().titleField}
+                                                 urlField={getConfig().urlField}
+                                                 resultView={ResultView}
+                                                 shouldTrackClickThrough={true}
+                                        />
+                                    }
+                                    bodyHeader={
+                                        <React.Fragment>
+                                            {wasSearched && <PagingInfo/>}
+                                            {wasSearched && <ResultsPerPage/>}
+                                        </React.Fragment>
+                                    }
+                                    bodyFooter={<Paging/>}
+                                />
+                            </ErrorBoundary>
+                        </div>
+                    );
+                }}
+            </WithSearch>
+        </SearchProvider>
+    );
 }
