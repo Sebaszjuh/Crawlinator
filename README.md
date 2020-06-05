53f5672537b0968f85c5d47f229248cb2aa7b38e
griends1 <Steelpan1>
2020-06-05 23:33:29 +0200

    Adding .env voor private key, adjustment readme file

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 - Go to [localhost:3002](http://localhost:3002)
 - Create an engine called: `crawlinator`
 - Go to [http://localhost:3002/as#/credentials](http://localhost:3002/as#/credentials)
-- Copy the private key and replace it in `crawlinator/pipelines.py`
+- Copy the private key and replace it in `.env`
 - Copy the search key and replace tin in `crawlinator-ui/src/config/engine.json`
 
 You only have to do the above steps once everytime you run `docker-compose up`
--- crawlinator/pipelines.py
+++ crawlinator/pipelines.py
@@ -9,16 +9,20 @@ from elastic_app_search import Client
 from scrapy.utils.markup import replace_tags, remove_tags_with_content, replace_escape_chars
 from scrapy.selector import Selector
 import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
 
 
 class appSearchPipeline(object):
     def process_item(self, item, spider):
         client = Client(
-            base_endpoint='localhost:3002/api/as/v1',
-            api_key='private-yvmokwy6ab1mc4tw45c66e1o',
+            base_endpoint=os.getenv("ENDPOINT"),
+            api_key=os.getenv("DATABASE_KEY"),
             use_https=False
         )
-        engine_name = 'crawlinator'
+        engine_name = os.getenv("ENGINE_NAME")
         client.index_document(engine_name, dict(item))
         logging.info(f'Item sent to App Search: {item["title"]}')
         return item
