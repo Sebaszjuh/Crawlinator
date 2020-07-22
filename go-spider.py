23202da3417c77aa6713746869c4931d1573070e
S. van de Griendt <sebastiaan.van.de.griendt@hva.nl>
2020-07-22 19:31:16 +0200

    Github Ready
    Signed-off-by: griends1 <Steelpan1>

--- go-spider.py
+++ go-spider.py
@@ -1,10 +1,7 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-from crawlinator.spiders.ziggo import ZiggoSpider
 from crawlinator.spiders.tor import TorSpider
-from crawlinator.spiders.login import LoginSpider
-from crawlinator.spiders.jonkodonko import jonkodonkoSpider
 
 process = CrawlerProcess(get_project_settings())
 process.crawl(TorSpider)
